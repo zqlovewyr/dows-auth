c43e701c32ee45841b12dcd7e021ba5feba32c11
guofulei <guofulei@qq.com>
2023-03-08 22:10:02 +0800

    add by gfl isvCreate 调整

--- auth-boot/src/main/java/org/dows/auth/boot/AuthBoot.java
+++ auth-boot/src/main/java/org/dows/auth/boot/AuthBoot.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"org.dows.auth"})
+@ComponentScan(basePackages = {"org.dows.auth.*"})
 public class AuthBoot {
 }
